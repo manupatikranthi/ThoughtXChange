c48f7496aaa08941a080d7f6f28f1002fe029985
unknown <kalorirohit@msitprogram.net>
2020-09-13 15:33:54 +0530

    Form UI built

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { Modal, Button, Form } from "react-bootstrap";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +22,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function NavBar() {
   const classes = useStyles();
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleFormSubmit = (event) => {
+    console.log(document.getElementById("formEmail").value);
+    setShow(false);
+  };
 
   return (
     <div className={classes.root}>
@@ -37,9 +47,58 @@ export default function NavBar() {
           <Typography variant="h6" className={classes.title}>
             ThoughtXChange
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={handleShow}>
+            Register
+          </Button>
         </Toolbar>
       </AppBar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Register</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form id="registrationForm" onSubmit={handleFormSubmit}>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formFullName">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control type="text" placeholder="Full name" />
+            </Form.Group>
+
+            <Form.Group controlId="formAge">
+              <Form.Label>Age</Form.Label>
+              <Form.Control type="number" placeholder="Age" />
+            </Form.Group>
+
+            <Form.Group controlId="formUserName">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="text" placeholder="User name" />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleFormSubmit}>
+            submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
